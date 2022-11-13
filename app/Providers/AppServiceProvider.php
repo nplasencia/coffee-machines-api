7c5c1eadef21b8a620d8b6c17c722bc288ccd597
Nauzet Plasencia <nauzet.plasencia.cruz@gmail.com>
2022-11-13 21:50:17 +0100

    Add proper bindings between interfaces and concrete classes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Gateways\BeanContainerFilesystemGateway;
+use App\Gateways\ContainerGatewayInterface;
+use App\Gateways\WaterContainerFilesystemGateway;
+use App\Services\BeansContainer;
+use App\Services\BeansContainerService;
+use App\Services\EspressoMachineInterface;
+use App\Services\EspressoMachineService;
+use App\Services\WaterContainer;
+use App\Services\WaterContainerService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +22,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(EspressoMachineInterface::class, EspressoMachineService::class);
+
+        $this->app->bind(WaterContainer::class, WaterContainerService::class);
+
+        $this->app->bind(BeansContainer::class, BeansContainerService::class);
+
+        $this->app->when(WaterContainerService::class)
+            ->needs(ContainerGatewayInterface::class)
+            ->give(WaterContainerFilesystemGateway::class);
+
+        $this->app->when(BeansContainerService::class)
+            ->needs(ContainerGatewayInterface::class)
+            ->give(BeanContainerFilesystemGateway::class);
     }
 }
